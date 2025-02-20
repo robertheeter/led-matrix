66b47d96191eaa2e2815b6938bf368126ce82d27
Robert Heeter <robert.heeter@princeton.edu>
2025-02-19 23:30:16 -0500

    update app selection

--- src/code.py
+++ src/code.py
@@ -1,26 +1,17 @@
 # type: ignore
 
 import os
-import board
 import time
-import touchio
+import json
 
 
 # PARAMETERS
 VERBOSE = False # print data
 
-APP_LIST = [
-    "app/subway/app.py",
-    "app/spotify/app.py"
-    ] # list of app file names
-
-APP_INDEX_DEFAULT = 0 # default app index
+APP_PATH = "/app.json"
 
 LOG_PATH = "/code_out.txt" # log file path
 MAX_LOG_SIZE = 50000 # max log file size [bytes]
-
-SETUP_DURATION = 10 # duration of setup mode [seconds]
-SETUP_LATENCY = 0.1 # latency (wait) between button presses [seconds]
 
 ERROR_LATENCY = 30 # latency (wait) after error before retrying [seconds]
 
@@ -35,44 +26,25 @@ if log_size > MAX_LOG_SIZE:
     os.remove(LOG_PATH)
 
 
-# DEVICE SETUP
-button_up = touchio.TouchIn(board.BUTTON_UP)
-button_down = touchio.TouchIn(board.BUTTON_DOWN)
+# APP SELECTION
+with open(APP_PATH, 'r') as file:
+    data = json.load(file) # read previous app from json file
 
-app_index = APP_INDEX_DEFAULT
-start_time = time.time()
+app_list = data['app_list']
+previous_app = data['previous_app']
+previous_app_index = app_list.index(previous_app)
 
-while True:
-    if button_up.value:
-        app_index += 1
-        if VERBOSE:
-            print("BUTTON UP")
-        while button_up.value:
-            time.sleep(SETUP_LATENCY)
-    
-    if button_down.value:
-        app_index -= 1
-        if VERBOSE:
-            print("BUTTON DOWN")
-        while button_down.value:
-            time.sleep(SETUP_LATENCY)
-    
-    if app_index > (len(APP_LIST) - 1):
-        app_index = len(APP_LIST) - 1
-    elif app_index < 0:
-        app_index = 0
+if previous_app_index == len(app_list) - 1: # select next sequential app
+    app = app_list[0]
+else:
+    app = app_list[previous_app_index + 1]
 
-    time.sleep(SETUP_LATENCY)
-
-    if VERBOSE:
-        print(f"app_index: {app_index}")
-    
-    if time.time() - start_time > SETUP_DURATION:
-        break
+with open(APP_PATH, 'w') as file:
+    data = {'previous_app': app, 'app_list': app_list}
+    json.dump(data, file) # write current app to json file
 
 
 # START SELECTED APP
-app = APP_LIST[app_index]
 print(f"app execution: {app}")
 
 complete = False
